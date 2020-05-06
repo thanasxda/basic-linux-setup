2c58b28dd6dfef550f599e84911738fb82294e65
thanasxda <15927885+thanasxda@users.noreply.github.com>
2020-05-06 22:56:35 +0300

    improvements to kde preconfiguration

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ source="$(pwd)"
 basicsetup=$source/.basicsetup
 ### set bash colors
 magenta="\033[05;1;95m"
-yellow="\033[1;93m" 
+yellow="\033[1;93m"
 restore="\033[0m"
 echo -e "${magenta}"
 echo ".::BASIC-LINUX-SETUP::. - mainly for (K)ubuntu focal/groovy"
@@ -38,7 +38,7 @@ sudo apt -f install -y ureadahead kexec-tools
 yes | sudo dpkg-reconfigure kexec-tools
 
 ### your git name & email - unhash and set up for personal usage
-sudo apt -f install -y git curl 
+sudo apt -f install -y git curl
 #gitname=thanasxda
 #gitmail=15927885+thanasxda@users.noreply.github.com
 #git config --global user.name $gitname
@@ -59,6 +59,9 @@ echo -e "${restore}"
 cd $basicsetup && mkdir -p tmp
 unzip -o basicsetup.zip -d $basicsetup/tmp
 cd $basicsetup/tmp
+### copy wallpaper
+sudo mv malakas.jpg /malakas.jpg
+### copy kde optimal preconfiguration
 sudo \cp -rf .local/ ~/
 sudo \cp -rf .config/ ~/
 ### installation firefox addons, install as firefox opens. close firefox and reclick on console
@@ -66,19 +69,21 @@ echo -e "${magenta}"
 echo INSTALL FIREFOX ADDONS ONE BY ONE, AFTER CLOSE FIREFOX AND CLICK ON CLI TILL ALL ADDONS ARE INSTALLED!!!
 echo -e "${restore}"
 sudo pkill firefox
-echo sorry for that firefox crash. part of setup... 
+echo sorry for that firefox crash. part of setup...
 cd .mozilla/firefox/.default-release
+### copy firefox advanced settings and enable hw acceleration
 sudo \cp -rf prefs.js ~/firefox/*.default-release/prefs.js
 sudo rm -rf $basicsetup/tmp
 cd $source
-yes | firefox https://addons.mozilla.org/firefox/downloads/file/3539016/adblock_plus-* 
-yes | firefox https://addons.mozilla.org/firefox/downloads/file/3560936/duckduckgo_privacy_essentials-* 
-yes | firefox https://addons.mozilla.org/firefox/downloads/file/3502002/youtube_audio_only-* 
-yes | firefox https://addons.mozilla.org/firefox/downloads/file/3053229/adblocker_for_youtubetm-* 
-yes | firefox https://addons.mozilla.org/firefox/downloads/file/3547657/hotspot_shield_free_vpn_proxy_unlimited_vpn-* 
-yes | firefox https://addons.mozilla.org/firefox/downloads/file/3550879/plasma_integration-* 
+### install firefox modules
+yes | firefox https://addons.mozilla.org/firefox/downloads/file/3539016/adblock_plus-*
+yes | firefox https://addons.mozilla.org/firefox/downloads/file/3560936/duckduckgo_privacy_essentials-*
+yes | firefox https://addons.mozilla.org/firefox/downloads/file/3502002/youtube_audio_only-*
+yes | firefox https://addons.mozilla.org/firefox/downloads/file/3053229/adblocker_for_youtubetm-*
+yes | firefox https://addons.mozilla.org/firefox/downloads/file/3547657/hotspot_shield_free_vpn_proxy_unlimited_vpn-*
+yes | firefox https://addons.mozilla.org/firefox/downloads/file/3550879/plasma_integration-*
 
-### configure swap 
+### configure swap
 echo -e "${yellow}"
 echo CONFIGURE SWAP
 echo -e "${restore}"
@@ -96,7 +101,7 @@ sudo sysctl vm.swappiness=10
 ### add i386 architecture needed for env
 sudo dpkg --add-architecture i386
 
-### grub config & system optimization 
+### grub config & system optimization
 echo -e "${yellow}"
 echo GRUB CONFIG
 echo -e "${restore}"
@@ -120,13 +125,13 @@ cd $git
 git clone https://github.com/akhilnarang/scripts.git
 cd scripts/setup
 Keys.ENTER | ./android_build_env.sh
-Keys.ENTER | ./ccache.sh 
+Keys.ENTER | ./ccache.sh
 
 ### setup repos #####################################################################################
 echo -e "${yellow}"
 echo SET UP REPOS
 echo -e "${restore}"
-### repos needed to contain all packages - set underneath repos according to your distro if necessary 
+### repos needed to contain all packages - set underneath repos according to your distro if necessary
 source_1='deb http://archive.canonical.com/ubuntu/ focal partner'
 source_2='deb http://gr.archive.ubuntu.com/ubuntu/ bionic main'
 source_3='deb http://gr.archive.ubuntu.com/ubuntu/ eoan main universe'
@@ -148,8 +153,8 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1378B444
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 
 ### mesa drivers and extras
-sudo add-apt-repository -y ppa:oibaf/graphics-drivers 
-sudo add-apt-repository -y ppa:git-core/ppa 
+sudo add-apt-repository -y ppa:oibaf/graphics-drivers
+sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:team-xbmc/ppa
 ### fix groovy distro syncing for now
 #file_4=/etc/apt/sources.list.d/team-xbmc-ubuntu-ppa-*.list
@@ -183,7 +188,7 @@ sudo apt -f install -y aptitude
 sudo aptitude -f install -y amd64-microcode android-sdk android-tools-adb android-tools-fastboot autoconf autoconf-archive autogen automake autopoint autotools-dev bash bc binfmt-support binutils-dev bison build-essential bzip2 ca-certificates ccache clang clang-11 clang-11-doc clang-format clang-format-11 clang-tidy clang-tools-11 clangd clangd-11 cmake curl dash desktop-base dkms dpkg-dev ecj expat fastjar file flatpak flex g++ gawk gcc gcc-10 gdebi gedit gettext git git-svn gnupg gparted gperf gstreamer1.0-qt5 help2man imagemagick intel-microcode java-propose-classpath kubuntu-restricted-extras kwrite lib32ncurses-dev lib32readline-dev lib32z1 lib32z1-dev libbz2-dev libc++-11-dev libc++abi-11-dev libc6-dev libc6-dev-i386 libcap-dev libclang-11-dev libclang-dev libclang1 libclang1-11 libelf-dev libexpat1-dev libffi-dev libfuzzer-11-dev libghc-bzlib-dev libgl1-mesa-dev libgmp-dev libjpeg8-dev libllvm-11-ocaml-dev libllvm-ocaml-dev libllvm11 liblz4-1 liblz4-1:i386 liblz4-dev liblz4-java liblz4-jni liblz4-tool liblzma-dev liblzma-doc liblzma5 libmpc-dev libmpfr-dev libncurses-dev libncurses5 libncurses5-dev libomp-11-dev libsdl1.2-dev libssl-dev libtool libtool-bin libvdpau-va-gl1 libvulkan1 libx11-dev libxml2 libxml2-dev libxml2-utils linux-libc-dev linux-tools-common lld lld-11 lldb llvm llvm-11 llvm-11-dev llvm-11-doc llvm-11-examples llvm-11-runtime llvm-dev llvm-runtime lzma lzma-alone lzma-dev lzop m4 make maven mesa-opencl-icd mesa-va-drivers mesa-vulkan-drivers nautilus ninja-build ocl-icd-libopencl1 openjdk-8-jdk openssh-client openssh-server optipng patch pigz pkg-config pngcrush python-all-dev python-clang python3.8 python3-distutils qt5-default rsync schedtool shtool snapd squashfs-tools subversion tasksel texinfo txt2man ubuntu-restricted-extras unzip vdpau-driver-all vlc vulkan-utils wget x11proto-core-dev xsltproc yasm zip zlib1g-dev
 
 ### SELECT EXPLICITLY FOR KDE PLASMA DESKTOP ENVIRONMENT!!! needs manual enabling from within settings
-sudo apt install -y plasma-workspace-wayland kwayland-integration wayland-protocols  
+sudo apt install -y plasma-workspace-wayland kwayland-integration wayland-protocols
 ### allow root privilege under wayland and supress output
 sudo sed -i '4s/.*/xhost +si:localuser:root >/dev/null/' /etc/default/grub
 #####################################################################################################
@@ -211,7 +216,7 @@ sudo apt -f install -y && sudo apt --fix-broken install -y
 rm -rf ukuu*
 
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo dpkg -i teamviewer* 
+sudo dpkg -i teamviewer*
 sudo apt -f install -y && sudo apt --fix-broken install -y
 rm -rf teamviewer*
 
@@ -251,7 +256,7 @@ cd $tc
 git clone https://github.com/TwistedPrime/twisted-clang.git
 mv twisted-clang clang
 
-### llvm binutils gcc buildscripts 
+### llvm binutils gcc buildscripts
 cd $git
 git clone https://github.com/ClangBuiltLinux/tc-build.git
 git clone https://github.com/USBhost/build-tools-gcc.git
@@ -292,9 +297,6 @@ echo -e "${restore}"
 cd $git
 git clone --depth=1 https://github.com/thanasxda/thanas-x86-64-kernel.git
 cd thanas-x86-64-kernel
-./build.sh 
+./build.sh
 
 ### DONE
-
-
-
