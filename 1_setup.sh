3d3030c13e9a544f2407340149e7a5e0ed5de135
thanasxda <15927885+thanasxda@users.noreply.github.com>
2020-06-20 02:11:31 +0300

    stuff

--- 1_setup.sh
+++ 1_setup.sh
@@ -593,7 +593,7 @@ fi
 sleep 5
 sudo apparmor_parser -r /etc/apparmor.d/*snap-confine*
 sudo apparmor_parser -r /var/lib/snapd/apparmor/profiles/snap-confine*
-
+sudo snap install ngrok
 
 ###### GITHUB REPOSITORIES ##########################################################################
 #####################################################################################################
